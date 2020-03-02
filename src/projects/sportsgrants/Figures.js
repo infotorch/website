fd4c315e4a7b0602a7f208594d77982f55427b52
Jack Nutt <jack@infotorch.org>
2020-03-02 21:36:27 +0000

    add figures for sports grants

--- src/projects/sportsgrants/Figures.js
+++ src/projects/sportsgrants/Figures.js
@@ -3,12 +3,17 @@ import Paper from "@material-ui/core/Paper"
 import Typography from "@material-ui/core/Typography"
 import "./index.css"
 
+import fig1 from "./figures/funding-01.png"
+import fig2 from "./figures/funding-02.png"
+
 const Data = () => (
   <div className="App">
     <Paper style={{ padding: "10px" }}>
       <Typography variant="h4" gutterBottom>
         Sports Grants Charts
       </Typography>
+      <img src={fig1} alt="fig1" />
+      <img src={fig2} alt="fig2" />
     </Paper>
   </div>
 )
